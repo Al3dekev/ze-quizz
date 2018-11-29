5cb935399cf2086067aa8bf63c187c17fd9c431a
Al3dekev <quentin1998@me.com>
2018-11-29 14:44:58 +0100

    project initialization

---
+++ src/main/java/com/github/al3dekev/zequizz/ZeQuizzApplication.java
@@ -0,0 +1,12 @@
+package com.github.al3dekev.zequizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeQuizzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeQuizzApplication.class, args);
+    }
+}
